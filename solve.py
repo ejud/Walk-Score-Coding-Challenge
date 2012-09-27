c37614774887f1f61f21aaa3ad588cab7b32f788
Ethan Jud <ethanpjud@gmail.com>
2012-09-27 19:58:35 -0400

    Factored graph into a class for convenience

--- solve.py
+++ solve.py
@@ -1,22 +1,40 @@
 import sys
 
+class Graph:
+    """A mutable, unweighted, directed graph type"""
+    def __init__(self):
+        self.graph = dict()
+        
+    def addEdge(self, a, b):
+        """Adds a single edge to the graph"""
+        if self.graph.has_key(a):
+            self.graph[a].add(b)
+        else:
+            self.graph[a] = set(b)
+    
+    def addEdges(self, edges):
+        """Adds edges from the provided collection of tuples.
+        Each tuple's first value is an origin node and its second
+        value is the corresponding destination."""
+        for edge in edges:
+            self.addEdge(edge[0], edge[1])
+            
+    def edges(self):
+        """Generates edges represented by tuples"""
+        for a, others in self.graph.iteritems():
+            for b in others:
+                yield (a, b)
+
 # Read the edges from the standard input stream as tuples
 edges = [tuple(line.strip().split('\t')) for line in sys.stdin.readlines()]
 
 # Assemble the edges into a graph structure represented by a dictionary
-graph = dict()
-for edge in edges:
-    if graph.has_key(edge[0]):
-        graph[edge[0]].add(edge[1])
-    else:
-        graph[edge[0]] = set(edge[1])
+graph = Graph()
+graph.addEdges(edges)
 
 # TODO - Eliminate nodes with only one input and one output edge
 
-# Build the output collection of edges.
 # Disregard the ordering (for now, at least), since we have probably added additional edges
-#  at this point.
-edges = [(origin, dest) for origin, destinations in graph.iteritems() for dest in destinations]
 
-for edge in edges:
+for edge in graph.edges():
     print edge[0] + '\t' + edge[1]
