37dc2203afe591c3f40e25e503951bdc4171f513
Ethan Jud <ethanpjud@gmail.com>
2012-09-27 19:15:39 -0400

    Corrected oversight in method of building output edge list

--- solve.py
+++ solve.py
@@ -13,12 +13,10 @@ for edge in edges:
 
 # TODO - Eliminate nodes with only one input and one output edge
 
-# Print the edges back to the standard output stream
-# We want to preserve the ordering of the graph, but it was lost in the data structure.
-# For now, we'll use a less-than-optimal method, iterating over the original collection and
-#  asking the graph whether the edge exists.
-edges = [edge for edge in edges if graph.has_key(edge[0]) and edge[1] in graph[edge[0]]]
+# Build the output collection of edges.
+# Disregard the ordering (for now, at least), since we have probably added additional edges
+#  at this point.
+edges = [(origin, dest) for origin, destinations in graph.iteritems() for dest in destinations]
 
 for edge in edges:
     print edge[0] + '\t' + edge[1]
-
