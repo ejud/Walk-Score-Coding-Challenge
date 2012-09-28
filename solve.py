97f8d76049611fd2ef3c25ebc1e7676742bb0e71
Ethan Jud <ethanpjud@gmail.com>
2012-09-28 08:43:52 -0400

    Initial algorithm complete.
    This could still use some optimizations to reduce computational
    complexity.

--- solve.py
+++ solve.py
@@ -6,24 +6,62 @@ class Graph:
         self.graph = dict()
         
     def addEdge(self, a, b):
-        """Adds a single edge to the graph"""
+        """Adds a single edge to the graph if it does not already exist.
+        
+        Each node has a key in the graph dictionary, but may or may not
+        have target nodes associated with it.
+        
+        """
         if self.graph.has_key(a):
             self.graph[a].add(b)
         else:
             self.graph[a] = set(b)
+        
+        if not self.graph.has_key(b):
+            self.graph[b] = set()
     
     def addEdges(self, edges):
         """Adds edges from the provided collection of tuples.
+        
         Each tuple's first value is an origin node and its second
-        value is the corresponding destination."""
+        value is the corresponding destination.
+        
+        """
         for edge in edges:
             self.addEdge(edge[0], edge[1])
             
-    def edges(self):
+    def iterEdges(self):
         """Generates edges represented by tuples"""
         for a, others in self.graph.iteritems():
             for b in others:
                 yield (a, b)
+    
+    def nodes(self):
+        """Returns a list of all nodes"""
+        return self.graph.keys()
+    
+    def nodesForInputEdges(self, node):
+        """Nodes corresponding to the given node's input edges.
+        
+        Currently O(N), but we can probably do better.
+        
+        """
+        return [a for a, others in self.graph.iteritems() if node in others];
+    
+    def nodesForOutputEdges(self, node):
+        """Nodes corresponding to the given node's output edges"""
+        if self.graph.has_key(node):
+            return [b for b in self.graph[node]]
+    
+    def removeNode(self, node):
+        """Removes a node from the graph"""
+        if self.graph.has_key(node):
+            # Remove node as source
+            del self.graph[node]
+            
+            # Remove node as destination
+            for targets in self.graph.itervalues():
+                targets.discard(node)
 
 # Read the edges from the standard input stream as tuples
 def edges():
@@ -34,8 +72,24 @@ def edges():
 graph = Graph()
 graph.addEdges(edges())
 
-# TODO - Eliminate nodes with only one input and one output edge
+# Eliminate nodes with only one input and one output edge. For now, this is
+#  done in multiple passes. In each pass, nodes that should be deleted are
+#  removed, but the process may result in new nodes that should be deleted.
+# In the worst case, this is O(n^2) (assuming we fix the nodesForInputEdges
+#  attribute). Can we do better?
+needsAnotherPass = True
+while needsAnotherPass:
+    # We'll flag this as true if we delete any more.
+    needsAnotherPass = False
+    for node in graph.nodes():
+        inputNodes = graph.nodesForInputEdges(node)
+        outputNodes = graph.nodesForOutputEdges(node)
+        if len(inputNodes) == 1 and len(outputNodes) == 1:
+            graph.removeNode(node)
+            if inputNodes[0] != outputNodes[0]:
+                graph.addEdge(inputNodes[0], outputNodes[0])
+            needsAnotherPass = True
 
 # Disregard the ordering (for now, at least), since we have probably added additional edges
-for edge in graph.edges():
+for edge in graph.iterEdges():
     print edge[0] + '\t' + edge[1]
